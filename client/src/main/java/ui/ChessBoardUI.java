59d91c6ecc811b66e66c0179a4660659841c10d4
Tyler Blackham <tylerablackham@gmail.com>
2024-03-15 22:38:33 -0600

    phase5: implement ChessBoardUI methods

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -3,38 +3,109 @@ package ui;
 import chess.ChessBoard;
 import chess.ChessPiece;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class ChessBoardUI {
     ChessPiece[][] board;
-    Boolean isFacingWhite;
+    Boolean isFacingBlack;
+    PrintStream out;
 
     public static void main(String[] args){
-        ChessBoardUI chessBoardUI = new ChessBoardUI(new ChessBoard().getBoard(), true);
+        ChessBoard chessBoard = new ChessBoard();
+        chessBoard.resetBoard();
+        draw(chessBoard, true);
+        System.out.println();
+        draw(chessBoard, false);
+    }
+
+    public static void draw(ChessBoard chessBoard, Boolean isFacingBlack){
+        ChessBoardUI chessBoardUI = new ChessBoardUI(chessBoard.getBoard(), isFacingBlack);
         chessBoardUI.drawBoard();
     }
 
-    public ChessBoardUI(ChessPiece[][] board, Boolean isFacingWhite) {
+    public ChessBoardUI(ChessPiece[][] board, Boolean isFacingBlack) {
         this.board = board;
-        this.isFacingWhite = isFacingWhite;
+        this.isFacingBlack = isFacingBlack;
+        out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
     }
 
     public void drawBoard() {
-
+        drawHeaderFooter();
+        if (isFacingBlack) {
+            for (int i = board.length - 1; i >= 0; i--){
+                drawRow(board[7-i], i);
+            }
+        }
+        else {
+            for (int i = 0; i < board.length; i++) {
+                drawRow(board[7-i], i);
+            }
+        }
+        drawHeaderFooter();
     }
 
     private void drawHeaderFooter() {
-
+        String[] letters = !isFacingBlack ? new String[]{" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "} :
+                new String[]{" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "};
+        out.print(EscapeSequences.SET_BG_COLOR_YELLOW);
+        out.print(EscapeSequences.SET_TEXT_COLOR_BLACK);
+        out.print("   ");
+        for (String letter : letters){
+            out.print(letter);
+        }
+        out.print("   ");
+        out.println(EscapeSequences.RESET_BG_COLOR);
     }
 
-    private void drawRow() {
-
+    private void drawRow(ChessPiece[] row, int rowNum) {
+        out.print(EscapeSequences.SET_TEXT_COLOR_BLACK);
+        out.print(EscapeSequences.SET_BG_COLOR_YELLOW);
+        out.print(" " + (8 - rowNum) + " ");
+        if (isFacingBlack) {
+            for (int i = row.length - 1; i >= 0; i--){
+                drawCell(row[i], rowNum, i);
+            }
+        }
+        else {
+            for (int i = 0; i < row.length; i++) {
+                drawCell(row[i], rowNum, i);
+            }
+        }
+        out.print(EscapeSequences.SET_BG_COLOR_YELLOW);
+        out.print(EscapeSequences.SET_TEXT_COLOR_BLACK);
+        out.print(" " + (8 - rowNum) + " ");
+        out.println(EscapeSequences.RESET_BG_COLOR);
     }
 
-    private void drawCell() {
-
+    private void drawCell(ChessPiece piece, int rowNum, int colNum) {
+        if (rowNum % 2 == colNum % 2) {
+            out.print(EscapeSequences.SET_BG_COLOR_DARK_GREY);
+        }
+        else {
+            out.print(EscapeSequences.SET_BG_COLOR_LIGHT_GREY);
+        }
+        if (piece != null) {
+            out.print(switch (piece.getTeamColor()) {
+                case WHITE -> EscapeSequences.SET_TEXT_COLOR_WHITE;
+                case BLACK -> EscapeSequences.SET_TEXT_COLOR_BLACK;
+            });
+        }
+        out.print(returnStringRepresentation(piece));
     }
 
     private String returnStringRepresentation(ChessPiece piece) {
-
+        if (piece == null) {
+            return "   ";
+        }
+        return switch (piece.getPieceType()){
+            case KING -> " K ";
+            case QUEEN -> " Q ";
+            case BISHOP -> " B ";
+            case KNIGHT -> " N ";
+            case ROOK -> " R ";
+            case PAWN -> " P ";
+        };
     }
 
 }
