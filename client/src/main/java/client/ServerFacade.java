060b8ba1eb0722ace99284c4dc8d75088a2fb2b4
Tyler Blackham <tylerablackham@gmail.com>
2024-03-18 16:39:42 -0600

    phase5: stub out ServerFacade methods

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,58 @@
 package client;
 
+import com.google.gson.Gson;
+import com.sun.nio.sctp.NotificationHandler;
+import model.*;
+
+import javax.websocket.ContainerProvider;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class ServerFacade {
+    Session session;
+
+
+    public ServerFacade(String url) throws Exception {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/connect");
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message){}
+            });
+
+        } catch (URISyntaxException e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public String register(UserData userData){
+        return "";
+    }
+    public String login(LoginRequest loginRequest){
+        return"";
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest createGameRequest) {
+        return null;
+    }
+
+    public GameList listGames(AuthToken authToken) {
+        return null;
+    }
+
+    public void joinGame(JoinGameRequest joinGameRequest) {
+
+    }
+
+    public void logout(AuthToken authToken) {
+
+    }
+
 }
